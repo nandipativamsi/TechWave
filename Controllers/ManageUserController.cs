f6dfa48fbf4057c666d7653b65f0e46f50422299
nvamsi2647 <vnandipati@conestogac.on.ca>
2024-08-15 16:58:40 -0400

    Added Admin Manage user

---
+++ Controllers/ManageUserController.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TechWave.Models.DomainModel;
+using TechWave.Models.ViewModel;
+
+namespace TechWave.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public ManageUserController(UserManager<User> userMngr, RoleManager<IdentityRole> roleMngr)
+        {
+            userManager = userMngr;
+            roleManager = roleMngr;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = new List<User>();
+            foreach (var user in userManager.Users)
+            {
+                user.RoleNames = await userManager.GetRolesAsync(user);
+                users.Add(user);
+            }
+
+            var model = new UserViewModel
+            {
+                Users = users
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var result = await userManager.DeleteAsync(user);
+                if (!result.Succeeded)
+                {
+                    string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description));
+                    TempData["message"] = errorMessage;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToAdmin(string id)
+        {
+            var adminRole = await roleManager.FindByNameAsync("Admin");
+            if (adminRole == null)
+            {
+                TempData["message"] = "Admin role does not exist. Click 'Create Admin Role' button to create it.";
+            }
+            else
+            {
+                var user = await userManager.FindByIdAsync(id);
+                await userManager.AddToRoleAsync(user, adminRole.Name);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromAdmin(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            await userManager.RemoveFromRoleAsync(user, "Admin");
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            await roleManager.DeleteAsync(role);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAdminRole()
+        {
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    UserName = model.Username,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userViewModel = new EditUserViewModel
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            return View(userViewModel);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Email = model.Email;
+            user.PhoneNumber = model.PhoneNumber;
+
+            var result = await userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Failed to update user.");
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
